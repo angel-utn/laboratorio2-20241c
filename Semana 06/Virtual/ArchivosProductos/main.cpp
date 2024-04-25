4e1aaba49cf8c5a93ef6b1dc59602f18fdabfb7f
Brian Lara <brian.lara@outlook.com.ar>
2024-04-25 19:53:31 -0300

    Archivos Producto Virtual

---
+++ Semana 06/Virtual/ArchivosProductos/main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include "Producto.h"
+
+using namespace std;
+
+int main () {
+   
+   
+   
+   
+   
+   
+   
+   /*
+   int v[5]{1,2,3,4,5};
+   Fecha fechas[5]{
+      Fecha(2,6,1990),
+      Fecha(3,5,2010),
+      Fecha(2,6,1990),
+      Fecha(3,5,2010),
+      Fecha(2,6,1990),
+   };
+   
+   ///foreach
+   for(Fecha f : fechas){
+      cout << f.toString() << endl;   
+   }  
+   */
+   
+   
+	
+	return 0;
+}
+
